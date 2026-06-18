529e5639faf2e50682bf17ef0bb2f0611d3651fd
praizo <tzpraise@gmail.com>
2026-06-18 16:45:56 +0100

    feat: register event listeners, define platform gates, and configure API rate limiting in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,7 +32,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if (class_exists(TelescopeServiceProvider::class)) {
+       if (class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
         }
     }
